7429503e1d5fd6bc94ce274fb401e4c9f5a8c1e4
Your Name <you@example.com>
2025-09-08 21:02:39 -0700

    [ADD]_A.Collatz Conjecture

---
+++ codeforces/A.collatz_conjecture.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+
+using namespace std;
+int main()
+{
+    int t;
+    cin>t;
+    while(t--)
+    {
+        int k,x;
+        cin>>k>>x;
+        for(int i=1;i<k;i++)
+        {
+            if(x%2==0)
+
+        }
+
+
+    }
+}
