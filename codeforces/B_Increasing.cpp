88316655b3cbc757c9cffb7dc0fa00090c651f8f
Your Name <you@example.com>
2025-09-18 03:23:14 -0700

    [ADD]_B.Increasing

---
+++ codeforces/B_Increasing.cpp
@@ -0,0 +1,38 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+
+        }
+        sort(a,a+n);
+       bool possible=true;
+         for(int i=0;i<n;i++)
+        {
+            if(a[i]== a[i+1])
+            {
+                possible=false;
+            }
+        }
+
+       if(possible)
+       {
+           cout<<"YES"<<endl;
+       }
+       else
+        {
+           cout<<"NO"<<endl;
+       }
+    }
+
+}
+
