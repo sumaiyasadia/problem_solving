928d78242e4ff5e8f9c39ee1d85a4b4b118cf168
Your Name <you@example.com>
2025-09-18 02:47:10 -0700

    [ADD]_B. Equal Candies

---
+++ codeforces/B_equal_candies.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n];
+        for(int i=0;i<n;i++)
+        {
+            cin>>a[i];
+
+        }
+        sort(a,a+n);
+        int total=0;
+         for(int i=1;i<n;i++)
+        {
+            total+=a[i]-a[0];
+
+        }
+        cout<<total<<endl;
+    }
+
+}
