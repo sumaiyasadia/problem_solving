b84218882ae1d0e3f28d43bd66fe53e934bb707d
sumaiyasadia <sadiasumaiyaislam99@gmail.com>
2026-04-16 10:26:26 +0600

    [ADD]_B.Flip the Bit(Easy version)

---
+++ codeforces/B. Flip the Bit (Easy Version).cpp
@@ -0,0 +1,29 @@
+#include<bits/stdc++.h>
+using namespace std;
+void solve()
+{
+    int n,k;
+    cin>>n>>k;
+    int a[n];
+k--;
+    for(int i=0;i<n;i++)
+    {
+        cin>>a[i];
+    }
+    int ans=0;
+    for(int i=0;i<k;i++)
+    {
+        if()
+    }
+    cout<<endl;
+}
+int main()
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        solve();
+    }
+
+}
